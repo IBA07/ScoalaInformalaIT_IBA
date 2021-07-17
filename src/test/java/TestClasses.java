f6d5e742142430a0f3b7a273a5c50a4181c26b3e
IBA07 <ion.badea.07@gmail.com>
2021-07-17 10:24:27 +0300

    V01

---
+++ src/test/java/TestClasses.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestClasses {
+
+    @Test
+    public void Test01() {
+        System.out.println("This is my very first test !");
+    }
+}
---
+++ src/test/java/TestNgClassses.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+
+public class TestNgClassses {
+    @Test
+    public void Test01() {
+        System.out.println("This is my very first test with test ng !");
+    }
+
+}
